e3bdbe5c637863205267cf7a80fb9c62741c64b2
Anton Rolin <antonrolin@hotmail.com>
2020-11-17 18:59:31 +0100

    Added GameRound class

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -7,7 +7,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-    List<Question> gameTurns;
+    int rounds = 2;
+    int questionsPerRound = 2;
+    List<GameRound> gameRounds;
+    List<Question> questionList;
     Path filepath = Path.of("src/Server/questions");
 
     Game(Player player1, Player player2){
@@ -16,29 +19,37 @@ public class Game {
         readFileAddToList(filepath);
 
 
+
     }
 
     private void readFileAddToList(Path filepath) {
-        try(Scanner scan = new Scanner(filepath)) {
+        try (Scanner scan = new Scanner(filepath)) {
             scan.useDelimiter(";");
-            gameTurns = new ArrayList<>();
+            questionList = new ArrayList<>();
 
-            while(scan.hasNext()){
+            while (scan.hasNext()) {
                 String category = scan.next();
                 String prompt = scan.next();
                 String[] options = new String[4];
                 for (int i = 0; i < 4; i++) {
-                    options [i] = scan.next();
+                    options[i] = scan.next();
                 }
                 int correctAnswerIndex = scan.nextInt();
-                gameTurns.add(new Question(category, prompt, options, correctAnswerIndex));
+                questionList.add(new Question(category, prompt, options, correctAnswerIndex));
             }
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
+        private void playRound(){
+            for (int i = 0; i < rounds; i++) {
+                GameRound round = new GameRound(questionsPerRound, questionList);
+            }
+            return;
+        }
+
 
 }
---
+++ src/Server/GameRound.java
@@ -0,0 +1,44 @@
+package Server;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by: Anton Rolin
+ * Date: 17/11/2020
+ * Time: 18:24
+ * Project: Quizkampen
+ * Copyright: MIT
+ */
+public class GameRound {
+
+    List<Question> questionList = new ArrayList();
+
+    GameRound(int questionsPerRound, List<Question> questionList){
+        randomiseQuestions(questionsPerRound, questionList);
+
+    }
+
+    private List<Question> randomiseQuestions(int questionsPerRound, List<Question> questionList){
+        Collections.copy(this.questionList, questionList);
+        List<Question> roundQuestionList = new ArrayList();
+        Collections.shuffle(questionList);
+        Question q = questionList.get(0);
+        roundQuestionList.add(q);
+        this.questionList.remove(q);
+
+        int i = 1;
+        for (Question question: questionList) {
+            if(question.getCategory().equalsIgnoreCase(q.getCategory())) {
+                roundQuestionList.add(question);
+                this.questionList.remove(question);
+                i++;
+            }
+
+            if(i == questionsPerRound)
+                break;
+        }
+        return roundQuestionList;
+    }
+}
