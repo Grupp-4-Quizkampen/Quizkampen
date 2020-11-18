6d2b68979e4e04652eb302b10b49a221eeaed433
stefan-hennings <stefan.hennings4@gmail.com>
2020-11-18 09:15:07 +0100

    GameRounds are created in Game

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -8,13 +8,27 @@ import java.util.Scanner;
 
 public class Game {
     List<Question> gameTurns;
+    List<GameRound> rounds = new ArrayList<>();
+    int questionsPerRound = 2;
+    int numberOfRounds = 2;
+    Player player1;
+    Player player2;
     Path filepath = Path.of("src/Server/questions");
 
     Game(Player player1, Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
         player1.start(); //startar spelare 1
         player2.start(); //startar spelare 2
         readFileAddToList(filepath);
 
+        play();
+    }
+
+    private void play() {
+        for (int i = 0; i < numberOfRounds; i++) {
+            rounds.add(new GameRound(player1, player2, questionsPerRound));
+        }
 
     }
 
