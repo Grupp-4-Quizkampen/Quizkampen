c3d7dc55ef6957d8864e2c5ce2b03c12d887e556
Anton Rolin <antonrolin@hotmail.com>
2020-11-25 15:24:45 +0100

    GamePanel class added

--- src/Client/MainGUI.java
+++ src/Client/MainGUI.java
@@ -1,5 +1,7 @@
 package Client;
 
+import Server.Question;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -12,6 +14,13 @@ import java.awt.*;
  */
 public class MainGUI extends JFrame{
 
+    String[] answers = {
+            "Blå",
+            "Röd",
+            "Gul",
+            "Grön"
+    };
+
     private JPanel mainPanel = new JPanel();
     int width = 500;
     int height = 500;
@@ -20,7 +29,7 @@ public class MainGUI extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         mainPanel.setLayout(new BorderLayout());
-        mainPanel.add(BorderLayout.CENTER, new StartPanel());
+        mainPanel.add(BorderLayout.CENTER, new GamePanel(new Question("Färger","Vilken färg är mest blå?", answers, 0)));
         add(mainPanel);
         setSize(width, height);
     }
