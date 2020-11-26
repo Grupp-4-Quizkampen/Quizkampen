6f23534fa2ad3f0784567e8f481a6128d5db6610
Simon <simon_ekenberg@hotmail.com>
2020-11-26 13:45:09 +0100

    ResultPanel, first

--- src/Client/ResultPanel.java
+++ src/Client/ResultPanel.java
@@ -1,5 +1,8 @@
 package Client;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Simon Ekenberg
  * Date: 2020-11-18
@@ -7,5 +10,28 @@ package Client;
  * Project: IntelliJ IDEA
  * Copyright: MIT
  */
-public class ResultPanel {
+public class ResultPanel extends JPanel {
+    private JPanel scorePanel = new JPanel();
+    private JPanel namePanel = new JPanel();
+    private JLabel firstName = new JLabel("");
+    private JLabel secondName = new JLabel("");
+    JLabel firstPlayerScore = new JLabel("");
+    JLabel dash = new JLabel(" - ");
+    JLabel secondPlayerScore = new JLabel("");
+    JButton nextRoundButton = new JButton("Next Round");
+
+    ResultPanel(){
+        namePanel.setLayout(new FlowLayout());
+    namePanel.add(firstName);
+    namePanel.add(secondName);
+    scorePanel.setLayout(new BorderLayout());
+    firstPlayerScore.setText("");
+    dash.setText("");
+    secondPlayerScore.setText("");
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+
 }
