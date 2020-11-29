6b6ef8466ddc7d3423599271088fc62dd03e9b47
Karl Pettersson <karl.pettersson@yh.nackademin.se>
2020-11-29 01:58:51 +0100

    added currentRoundIndex to keep track of number of rounds

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -12,6 +12,7 @@ public class Game implements Serializable {
     List<GameRound> gameRounds = new ArrayList<>();
     List<Question> unusedQuestions;
     List<Question> questionList;
+    int currentRoundIndex = 0;
 
     PropertiesHandler propertiesHandler = new PropertiesHandler();
     int questionsPerRound = propertiesHandler.getNumberOfQuestions();
